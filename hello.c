121d700f88cd68a767c201a407dd5b656f70dc53
jmartinez75423 <142929714+jmartinez75423@users.noreply.github.com>
2023-08-25 17:48:21 -0500

    first commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Joshua Martinez!
+ * Date: 2023/08/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Josh!\n");
+  printf("Computer Science\n");
+  
+
+  return 0;
+}
